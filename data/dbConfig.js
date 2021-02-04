dbfbdfd59570bfbfa9f107859e27333f2599ddde
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2021-02-03 19:37:52 -0800

    feat(sql): create crud operations
    created simple CRUD operations with sql, no knex

---
+++ data/dbConfig.js
@@ -0,0 +1,12 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+   user: process.env.USER,
+   password: process.env.PASSWORD,
+   host: process.env.HOST,
+   port: process.env.PORT,
+   database: 'rowsql'
+});
+
+module.exports = pool;
+
---
+++ migrations/database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE rowsql;
+
+CREATE TABLE todo
+(
+   id SERIAL PRIMARY KEY,
+   description VARCHAR(255)
+);
